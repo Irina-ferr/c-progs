80d174ad180ed400a5c1fb6aea6f7ebfeb7f09aa
Irina <vega_is@mail.ru>
2024-09-22 17:34:57 +0400

    26

--- Программы на си/26.c
+++ Программы на си/26.c
@@ -1,1 +1,25 @@
-//Напечатайте свое имя в углах экрана.
+//Выведите имя в углах экрана
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    const char* name = "IRINA";
+
+    // Получаем ширину консоли (можно задать вручную)
+    int consoleWidth = 80; // Задайте нужную ширину консоли
+    int consoleHeight = 20; // Задайте нужную высоту консоли
+
+    // Печатаем имя в углах консоли
+    printf("%-*s\n", consoleWidth, name); // Верхний левый угол
+    printf("%*s\n", consoleWidth - strlen(name), name); // Верхний правый угол
+
+    // Печатаем пустые строки до нижнего края
+    for (int i = 1; i < consoleHeight - 1; i++) {
+        printf("\n");
+    }
+
+    printf("%-*s\n", consoleWidth, name); // Нижний левый угол
+    printf("%*s\n", consoleWidth - strlen(name), name); // Нижний правый угол
+
+    return 0;
+}
